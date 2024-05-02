134320ab74cd7c5d3d5f27c7b20ea0134ce76eb0
Devang Asnadia <141860084+devangasnadia@users.noreply.github.com>
2024-05-01 21:37:23 -0400

    Add files via upload

---
+++ js/artists.js
@@ -0,0 +1,37 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  {
+    id: "a1",
+    name: "Aditya Gadhvi",
+    links: [
+      { url: "https://www.instagram.com/adityagadhviofficial/", name: "Instagram" },
+      { url: "https://twitter.com/AdityaGadhvi03", name: "Twitter" },
+      { url: "https://soundcloud.com/search?q=aditya%20gadhvi&query_urn=soundcloud%3Asearch-autocomplete%3A4d41d03d7bf04530a0df7551ca8e377f", name: "SoundCould" }
+    ]
+  },
+
+  {
+    id: "a2",
+    name: "Kirtidan Gadhvi",
+    links: [
+      { url: "https://www.instagram.com/kirtidangadhviofficial/", name: "Instagram" },
+      { url: "https://twitter.com/KirtidanGadhvi", name: "Twitter" },
+      { url: "https://soundcloud.com/search?q=kirtidan%20gadhvi&query_urn=soundcloud%3Asearch-autocomplete%3A3b4757a767a147b9bfd7af58007130b7", name: "SoundCould" }
+    ]
+  },
+
+  {
+    id: "a3",
+    name: "Arijit Singh",
+    links: [
+      { url: "https://www.instagram.com/arijitsingh/", name: "Instagram" },
+      { url: "https://twitter.com/arijitsingh", name: "Twitter" },
+      { url: "https://soundcloud.com/search?q=arijit%20singh&query_urn=soundcloud%3Asearch-autocomplete%3A9d4e7f8318a44783996e6b904db1f3a9", name: "SoundCould" }
+    ]
+  }
+];
---
+++ js/songs.js
@@ -0,0 +1,259 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    id: "s1",
+    artistId: "a1",
+    title: "Mahahetvali",
+    year: "2021",
+    duration: 243, 
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1615473986468-e3ebf02e9e3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwbW90aGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+  youtubeUrl: "https://youtu.be/3gUqUA00myo"
+  },
+  {
+    id: "s2",
+    artistId: "a1",
+    title: "Rang Morla",
+    year: "2023",
+    duration: 261,
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1534088568595-a066f410bcda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=451&q=80",
+  youtubeUrl: "https://youtu.be/mPLR7q7-_CY"
+  },
+  {
+    id: "s3",
+    artistId: "a1",
+    title: "Hansla",
+    year: "2015",
+    duration: 315,
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1566578312612-d903c67c4ad2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  youtubeUrl: "https://youtu.be/VDvyvITwPf0"
+  },
+  {
+    id: "s4",
+    artistId: "a1",
+    title: "Nagar Nandji Na Laal",
+    year: "2023",
+    duration: 345, 
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1625580916836-1badf2c15b32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+  youtubeUrl: "https://youtu.be/iraezTzB938"
+  },
+  {
+    id: "s5",
+    artistId: "a1",
+    title: "Rang Bhini Radha",
+    year: "2019",
+    duration: 257,
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1606293927185-20fad5f0a93e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+  youtubeUrl: "https://youtu.be/JYLEyMvj6sE"
+  },
+  {
+    id: "s6",
+    artistId: "a1",
+    title: "Aavi Rudi Ajwadi Raat",
+    year: "2022",
+    duration: 506, 
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1598972674627-30907e6ba976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  youtubeUrl: "https://youtu.be/zRQkYZYCpUc"
+  },
+  {
+    id: "s7",
+    artistId: "a1",
+    title: "Bhajan Vina Mari Bhukh Nai Bhange",
+    year: "2022",
+    duration: 254,
+    flagged: false,
+    imageUrl:
+    "https://images.unsplash.com/photo-1568219656418-15c329312bf1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  youtubeUrl: "https://youtu.be/llZu57g7v_g"
+  },
+  
+  ////////////// Artist - 2 /////////////////
+
+  {
+    id: "s1",
+    artistId: "a2",
+    title: "Rasiyo Rupalo Rang Reliyo",
+    year: "2023",
+    duration: 137,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/6a/66/26/6a662623f7ba400f3358ac039789c099.jpg",
+  youtubeUrl: "https://youtu.be/OhP4ryQCfZ8"
+  },
+
+  {
+    id: "s2",
+    artistId: "a2",
+    title: "Gori Radha Ne Kalo Kaan",
+    year: "2017",
+    duration: 178, 
+    flagged: true,
+    imageUrl:
+    "https://i.pinimg.com/564x/4f/10/15/4f10152142fe8c107bd525b49f18718e.jpg",
+  youtubeUrl: "https://youtu.be/ccqg6e2rfLU"
+  },
+  {
+    id: "s3",
+    artistId: "a2",
+    title: "Pidi Matudi Laviyane",
+    year: "2019",
+    duration: 602,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/46/3b/9c/463b9ca4a89e3990f85a7d1fc026ddca.jpg",
+  youtubeUrl: "https://youtu.be/UP0qHqpTbrw"
+  },
+  {
+    id: "s4",
+    artistId: "a2",
+    title: "Shiv Tandav Strotam",
+    year: "2021",
+    duration: 305,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/0b/c9/f5/0bc9f556bb7c49ce12353d94b332134e.jpg",
+  youtubeUrl: "https://youtu.be/Z1vlNxgZIMg"
+  },
+  {
+    id: "s5",
+    artistId: "a2",
+    title: "Jogi Aya",
+    year: "2021",
+    duration: 657, 
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/95/91/06/9591062ec741ac3a1acfebaa09b11722.jpg",
+  youtubeUrl: "https://youtu.be/GN13e1UTtB0"
+  },
+  {
+    id: "s6",
+    artistId: "a2",
+    title: "Laadki",
+    year: "2019",
+    duration: 307, 
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/27/23/fa/2723faa58cd9b61d662ff9b8ce8a4d8d.jpg",
+  youtubeUrl: "https://youtu.be/5U9uP6riAZM"
+  },
+  {
+    id: "s7",
+    artistId: "a2",
+    title: "Jam Kandorna",
+    year: "2019",
+    duration: 345, 
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/13/2e/9a/132e9ab1f3c48c5b39644375a5f9f60c.jpg",
+  youtubeUrl: "https://youtu.be/PKwNPKjLdZM"
+  },
+
+    ////////////// Artist - 3 /////////////////
+
+  {
+    id: "s1",
+    artistId: "a3",
+    title: "Deva Deva",
+    year: "2022",
+    duration: 192, 
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/bd/c8/37/bdc837fa030b21390eac3967ccc28720.jpg",
+  youtubeUrl: "https://youtu.be/WjAPDofGg28"
+  },
+
+  {
+    id: "s2",
+    artistId: "a3",
+    title: "Kesariya",
+    year: "2022",
+    duration: 173,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/e3/ea/63/e3ea63c19dc88c6c881b646b0f32c15c.jpg",
+  youtubeUrl: "https://youtu.be/BddP6PYo2gs"
+  },
+  {
+    id: "s3",
+    artistId: "a3",
+    title: "Apna Bana le",
+    year: "2023",
+    duration: 195,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/32/f4/61/32f4619367cc43cfeef43af09769dc58.jpg",
+  youtubeUrl: "https://youtu.be/ElZfdU54Cp8"
+  },
+  {
+    id: "s4",
+    artistId: "a3",
+    title: "Phir Aur Kya Chahiye",
+    year: "2023",
+    duration: 216,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/da/89/50/da8950a6f50ed58e9b1d227bd2136b96.jpg",
+  youtubeUrl: "https://youtu.be/8sLS2knUa6Y"
+  },
+  {
+    id: "s5",
+    artistId: "a3",
+    title: "Khairiyat",
+    year: "2020",
+    duration: 240,
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/cc/3c/ce/cc3cce6e54f036da63635f8fc177e646.jpg",
+  youtubeUrl: "https://youtu.be/9-AKLAfpjrI"
+  },
+  {
+    id: "s6",
+    artistId: "a3",
+    title: "Hamdard",
+    year: "2014",
+    duration: 257, 
+    flagged: true,
+    imageUrl:
+    "https://i.pinimg.com/564x/3d/b4/ee/3db4ee192f8b232d6505f22a8f687c3b.jpg",
+  youtubeUrl: "https://youtu.be/FJ55SHCzt88"
+  },
+  {
+    id: "s7",
+    artistId: "a3",
+    title: "Tum Hi Ho",
+    year: "2013",
+    duration: 249, 
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/d7/3d/7e/d73d7ead0c697367c39d5b4426a9e9e3.jpg",
+  youtubeUrl: "https://youtu.be/IJq0yyWug1k"
+  },
+  {
+    id: "s8",
+    artistId: "a3",
+    title: "Har Har Gange",
+    year: "2019",
+    duration: 210, 
+    flagged: false,
+    imageUrl:
+    "https://i.pinimg.com/564x/a2/84/30/a284307514683be8f64b38536aa71ee0.jpg",
+  youtubeUrl: "https://youtu.be/Njyx5ZuwEHI"
+  },
+
+];
